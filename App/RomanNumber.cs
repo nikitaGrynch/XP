f2b4f647d6b95b56a79c28986999a324de3cc44e
grynch <nikita.grynch@gmail.com>
2023-08-31 15:04:04 +0300

    HW_31_08_23

--- App/RomanNumber.cs
+++ App/RomanNumber.cs
@@ -2,5 +2,20 @@ namespace App;
 
 public class RomanNumber
 {
-    
+    public int Value { get; set; }
+    public static RomanNumber Parse(string input)
+    {
+        int value = 0;
+        switch (input)
+        {
+            case "I": value = 1;
+                break;
+            case "II": value = 2;
+                break;
+        }
+        return new()
+        {
+            Value = value
+        };
+    }
 }
