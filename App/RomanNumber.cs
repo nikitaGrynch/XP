86d551f2dff5219543cc0a61ff463755d00af41c
grynch <nikita.grynch@gmail.com>
2023-09-05 18:46:34 +0300

    HW_04_09_23

--- App/RomanNumber.cs
+++ App/RomanNumber.cs
@@ -2,20 +2,55 @@ namespace App;
 
 public class RomanNumber
 {
+    private Dictionary<String, int> romanNumbers = new()
+    {
+        { "I", 1 },
+        { "V", 5 },
+        { "X", 10 }
+    };
     public int Value { get; set; }
+
     public static RomanNumber Parse(string input)
     {
-        int value = 0;
-        switch (input)
+        if (String.IsNullOrEmpty(input))
         {
-            case "I": value = 1;
-                break;
-            case "II": value = 2;
-                break;
+            throw new ArgumentException("Empty input");
         }
+        int result = 0;
+        int prev = 0;
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            int current = input[i] switch
+            {
+                'I' => 1,
+                'V' => 5,
+                'X' => 10
+            };
+            result += (current < prev) ? -current : current;
+            prev = current;
+        }
+
         return new()
         {
-            Value = value
+            Value = result
         };
+        // int value = 0;
+        // switch (input)
+        // {
+        //     case "I": value = 1;
+        //         break;
+        //     case "II": value = 2;
+        //         break;
+        // }
+        //     return new()
+        //     { 
+        //         // Value = input.Length
+        //         // Value = input switch
+        //         // {
+        //         //     "I" => 1,
+        //         //     "II" => 2,
+        //         //     "III" => 3
+        //         // }
+        //     };
     }
 }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -8,6 +8,33 @@ public class UnitTest1
     [TestMethod]
     public void TestRomanNumberParse()
     {
+        Dictionary<String, int> testCases = new()
+        {
+            { "II", 2 },
+            { "III", 3 },
+            {"IIII", 4},
+            {"IV", 4},
+            {"V", 5},
+            {"VI", 6},
+            {"VII", 7},
+            {"VIII", 8},
+            {"VIIII", 9},
+            {"IX", 9},
+            {"X", 10},
+            {"XI", 11},
+            {"XII", 12},
+            {"XIII", 13},
+            {"XIIII", 14},
+            {"XIV", 14},
+            {"XV", 15},
+            {"XIIIII", 15},
+            {"XVI", 16},
+            {"XVII", 17},
+            {"XX", 20},
+            {"XXIIIII", 25},
+            {"XXV", 25},
+            {"XXX", 30},
+        };
         Assert.AreEqual(               // Элемент теста - увтерждение (Assert)
             1,                 // ожидаемое значение 
             RomanNumber          
@@ -15,12 +42,16 @@ public class UnitTest1
                 .Value,               // 
             "1 == I"
         );
-        Assert.AreEqual(      
-            2,                
-            RomanNumber       
-                .Parse("II")   
-                .Value,       
-            "2 == II"
-        );
+        foreach (var pair in testCases)
+        {
+            Assert.AreEqual(      
+                pair.Value,                
+                RomanNumber       
+                    .Parse(pair.Key)   
+                    .Value,       
+                $"{pair.Value} == {pair.Key}"
+            );
+        }
+
     }
 }
